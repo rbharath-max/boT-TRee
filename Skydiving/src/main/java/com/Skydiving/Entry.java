fbe4288f0053ae35b2a10fddd51c12edc7ac5af1
rbharath-max <81753407+rbharath-max@users.noreply.github.com>
2022-03-31 11:26:05 +0530

    Spring

---
+++ Skydiving/src/main/java/com/Skydiving/Entry.java
@@ -0,0 +1,24 @@
+package com.Skydiving;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Entry {
+	int serialnumber=12345678;
+
+	public int getSerialnumber() {
+		return serialnumber;
+	}
+
+	public void setSerialnumber(int serialnumber) {
+		this.serialnumber = serialnumber;
+	}
+	
+	
+	
+
+
+	
+	
+
+}
---
+++ Skydiving/src/test/java/com/Skydiving/SkydivingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Skydiving;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkydivingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
