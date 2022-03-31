1e6193b20930f524d0904e2f9822bd71a73ff360
rbharath-max <81753407+rbharath-max@users.noreply.github.com>
2022-03-31 11:42:15 +0530

    JFSspring

---
+++ JFSspring/src/Myspring/DriverClass.java
@@ -0,0 +1,20 @@
+package Myspring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class DriverClass {
+
+	public static void main(String[] args) {
+		ApplicationContext context=new ClassPathXmlApplicationContext("spelConfig.xml");
+		Book1 book1=(Book1)context.getBean("b1");
+		System.out.println(book1);
+		Book2 book2=(Book2)context.getBean("b2");
+		System.out.println(book2);
+
+
+
+
+	}
+
+}
